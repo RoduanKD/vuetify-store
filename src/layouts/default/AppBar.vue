a3c39dabb10f35eff96e3a0b8781a2a64424d4cf
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-12-28 16:36:45 +0300

    add cart link to navbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -8,18 +8,40 @@
       variant="text"
       to="/"
       exact
+      stacked
     >
       Home
     </v-btn>
     <v-btn
       variant="text"
       to="/products"
+      stacked
     >
       Explore Products
+    </v-btn>
+    <v-btn
+      variant="text"
+      to="/cart"
+      stacked
+    >
+      <v-badge
+        :content="itemsCount"
+        floating
+        color="error"
+      >
+        <v-icon>mdi-cart</v-icon>
+      </v-badge>
     </v-btn>
   </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+import { mapState } from 'pinia'
+import cart from '@/store/cart';
+
+export default {
+  computed: {
+    ...mapState(cart, ['itemsCount'])
+  }
+}
 </script>
