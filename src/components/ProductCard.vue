85877300307a7d91ef035f10bb2ddd1d36854fdb
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2022-12-28 16:36:28 +0300

    add "add to cart" button to product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,10 +2,21 @@
   <v-card>
     <v-card-title>{{ title }}</v-card-title>
     <v-card-text>{{ description }}</v-card-text>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        @click="addToCart({id, title, price, image, category})"
+      >
+        <v-icon>mdi-cart</v-icon> Add to cart
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import { mapActions } from 'pinia'
+import CartStore from '@/store/cart.js'
+
 export default {
   props: {
     id: {
@@ -36,6 +47,10 @@ export default {
       type: Object,
       required: true
     },
+  },
+
+  methods: {
+    ...mapActions(CartStore, ['addToCart'])
   }
 }
 </script>
