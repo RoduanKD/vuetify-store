d57f1b461e6389c2b10b2bf9f0484595d68afe22
Roduan Kareem Aldeen <37983260+RoduanKD@users.noreply.github.com>
2023-01-07 14:28:19 +0300

    axios installation & simple example

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import { mapState } from 'pinia';
 import user from '@/store/user';
 export default {
@@ -13,13 +14,14 @@ export default {
     ...mapState(user, ['user'])
   },
   created() {
-    fetch('https://dummyjson.com/users/' + this.user.id, {
+    axios.get('https://dummyjson.com/users/' + this.user.id, {
       headers: {
         Authorization: 'Bearer ' + this.user.token
       },
     })
-      .then(res => res.json())
-      .then(data => this.userInfo = data);
+      .then(response => {
+        this.userInfo = response.data
+      })
   },
 }
 </script>
